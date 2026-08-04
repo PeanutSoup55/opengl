9890f6f763743253c42ac5a1139781c2617ce08c
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-08-04 18:23:27 -0400

    exercise 1, needed to change the GLsizei to 6 to accomodate for teh 3 extra vertices

--- src/main.cpp
+++ src/main.cpp
@@ -53,10 +53,19 @@ int main() {
     }
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
+        -0.9f, 0.4f, 0.0f, 
         0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 
+
+        0.0f, -0.5f, 0.0f,
+        -0.3f, 0.4f, 0.0f,
+        -0.6f, -0.8f, 0.0f
     };
+
+    // unsigned int indices[] = { // note that we start from 0!
+    //     0, 1, 3, // first triangle
+    //     1, 2, 3 // second triangle
+    // };
 
 
     unsigned int vertexShader;
@@ -97,12 +106,18 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    //unsigned int VBO, VAO, EBO;
     unsigned int VBO, VAO;
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
+    //glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -115,8 +130,10 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
         
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
